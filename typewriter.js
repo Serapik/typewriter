3b81956af99d8effbd5dc07f37aa72ddddfc7c30
Fadime Serap Keskinler <fsekurt@gmail.com>
2021-11-30 17:35:48 -0500

    Animated Sentence

---
+++ typewriter.js
@@ -0,0 +1,21 @@
+// const sentence = "hello there from lighthouse labs";
+
+// let delay = 0;
+// for (const char of sentence) {
+//   setTimeout(() => {
+//   process.stdout.write(char);
+//   }, delay);
+//   delay += 50;
+// }
+// setTimeout(() => {
+// console.log('');} , (delay + 10));
+
+const sentence = "hello there from lighthouse labs";
+
+let delay = 0;
+for (const char of sentence) {
+  setTimeout(() => {
+  process.stdout.write(char);
+  }, delay);
+  delay += 50;
+}
